3b5a2e97f7e8b2b56672640c7543b4fb73455b5e
aidinnaemi <aidin_naemi@yahoo.com>
2017-01-20 19:25:50 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '304558448:AAHewllFO6C9xXR0uvypuVVq9CMqW-yJXmA');
+define('BOT_TOKEN', '319284258:AAHoS4Bd-Au44PVu_tqePF6mdB-f2MtMwBA');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
